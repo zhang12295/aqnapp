598a30a0c35305887e93c3e8495c7826068a89ac
LXC <798196071@qq.com>
2016-04-07 20:25:39 +0800

    完善搜索功能

--- src/com/njaqn/itravel/aqnapp/service/SearchService.java
+++ src/com/njaqn/itravel/aqnapp/service/SearchService.java
@@ -2,10 +2,17 @@ package com.njaqn.itravel.aqnapp.service;
 
 import java.util.HashMap;
 import java.util.List;
+import android.R.string;
 
 public interface SearchService {
 	public int searchInProvinces(String search);
+
 	public HashMap<String, Object> searchInCities(String search);
+
 	public List<String> searchInSpotsForComplete(String search);
+
 	public List<HashMap<String, Object>> searchInSpotsForResults(String search);
+
+	public List<HashMap<String, Object>> getNearbySpot(double Longitude,
+			double Latitude, int maxSpotNum);
 }
