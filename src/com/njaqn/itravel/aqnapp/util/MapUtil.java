647dc312944ffd4b98cbef1319dd22723547e62e
zhang12295 <1154524693@qq.com>
2016-04-26 17:58:52 +0800

    完善功能

--- src/com/njaqn/itravel/aqnapp/util/MapUtil.java
+++ src/com/njaqn/itravel/aqnapp/util/MapUtil.java
@@ -8,6 +8,7 @@ import org.json.JSONObject;
 
 import android.content.Context;
 import android.os.Bundle;
+import android.os.Handler;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.View.OnClickListener;
@@ -44,7 +45,7 @@ import com.njaqn.itravel.aqnapp.service.bean.JingDianBean;
 import com.njaqn.itravel.aqnapp.AppInfo;
 import com.njaqn.itravel.aqnapp.R;
 
-public class MapUtil 
+public class MapUtil
 {
     private Context ctx;
     private LocationClient cli;
@@ -54,12 +55,12 @@ public class MapUtil
     private AppInfo app;
     private Button btnLocation;
     private VoiceUtil vUtil;
-    //表示显示那种颜色
-    private int i =0;
+    // 表示显示那种颜色
+    private int i = 0;
     private ButtonClickListener btnClick;
     private AmService aService = new AmServiceImpl();
-    
-    public MapUtil(Context ctx,  AppInfo app, VoiceUtil vUtil)
+
+    public MapUtil(Context ctx, AppInfo app, VoiceUtil vUtil)
     {
 	this.ctx = ctx;
 	this.app = app;
@@ -71,11 +72,16 @@ public class MapUtil
     {
 	this.btnLocation = btnLocation;
     }
+
     /**
      * 功能：在地图上添加一个覆盖物
-     * @param iconResource 显示的图标资源
-     * @param point 经纬度
-     * @param info 额外信息
+     * 
+     * @param iconResource
+     *            显示的图标资源
+     * @param point
+     *            经纬度
+     * @param info
+     *            额外信息
      */
     public void setMapMarker(int iconResource, LatLng point, Bundle info)
     {
@@ -88,18 +94,22 @@ public class MapUtil
 
     /**
      * 在地图上添加标识信息
-     * @param point 经纬度
-     * @param info 显示在地图上的信息
+     * 
+     * @param point
+     *            经纬度
+     * @param info
+     *            显示在地图上的信息
      */
     public void setPopMarker(LatLng point, Bundle info)
     {
 	// 初始化View
-	LayoutInflater inflater = (LayoutInflater) ctx.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+	LayoutInflater inflater = (LayoutInflater) ctx
+		.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
 	View v = inflater.inflate(R.layout.am001_map_popview, null);
 
 	ImageButton button = (ImageButton) v.findViewById(R.id.btnPlayAudio);
-	ImageButton enter = (ImageButton) v.findViewById(R.id.btnEnter); 
-	if(btnClick != null)
+	ImageButton enter = (ImageButton) v.findViewById(R.id.btnEnter);
+	if (btnClick != null)
 	{
 	    btnClick = null;
 	}
@@ -115,54 +125,54 @@ public class MapUtil
 	InfoWindow mInfoWindow = new InfoWindow(v, point, -27);
 	map.showInfoWindow(mInfoWindow);
     }
-    
-    //为相应的button添加监听事件
+
+    // 为相应的button添加监听事件
     private final class ButtonClickListener implements OnClickListener
     {
 
-	private Bundle info; 
+	private Bundle info;
+
 	public ButtonClickListener(Bundle info)
 	{
 	    this.info = info;
 	}
 
 	@Override
-    	public void onClick(View v)
+	public void onClick(View v)
 	{
 	    switch (v.getId())
 	    {
-	    	case R.id.btnPlayAudio:
-	    	    if (vUtil.getPlayMode() == 1 || vUtil.getPlayMode() == 2)
-		    {
-			vUtil.playStop();
-		    }
-		    vUtil.playAudio(info.getString("intro"));
-		    break;
+	    case R.id.btnPlayAudio:
+		if (vUtil.getPlayMode() == 1 || vUtil.getPlayMode() == 2)
+		{
+		    vUtil.playStop();
+		}
+		vUtil.playAudio(info.getString("intro"));
+		break;
 
-	    	case R.id.btnEnter:
-	    	    
-	    	    if(info.getString("type").equals("spot"))
-	    	    {
-	    		map.clear();
-	    		try
-			{
-	    		    setJingDianPointer(info.getInt("id"));
-			}
-			catch (Exception e)
-			{
-			    e.printStackTrace();
-			}
-	    		
-	    	    }
-	    	   
+	    case R.id.btnEnter:
+
+		if (info.getString("type").equals("spot"))
+		{
+		    map.clear();
+		    try
+		    {
+			setJingDianPointer(info.getInt("id"));
+		    }
+		    catch (Exception e)
+		    {
+			e.printStackTrace();
+		    }
+
+		}
+
 		break;
 	    }
-	    
 
 	}
     }
 
-    //开启定位
+    // 开启定位
     public boolean setCurrLocation(MapView view)
     {
 	map = view.getMap();
@@ -175,7 +185,7 @@ public class MapUtil
 	return true;
     }
 
-    //位置信息监听回掉接口
+    // 位置信息监听回掉接口
     public class MyLocationListener implements BDLocationListener
     {
 	@Override
@@ -183,13 +193,12 @@ public class MapUtil
 	{
 	    if (location == null)
 		return;
-	    //收集当前位置信息
+	    // 收集当前位置信息
 	    MyLocationData locData = new MyLocationData.Builder()
 		    .accuracy(location.getRadius()).direction(100)
 		    .latitude(location.getLatitude())
 		    .longitude(location.getLongitude()).build();
 	    String city = location.getCity();
-	    String province = location.getProvince();
 	    app.setCity(city.substring(0, city.length() - 1));
 	    app.setLongitude(location.getLongitude());
 	    app.setLatitude(location.getLatitude());
@@ -203,21 +212,25 @@ public class MapUtil
 		isFristLoc = false;
 		locationLatLng = new LatLng(location.getLatitude(),
 			location.getLongitude());
-		//设置地图中心点及缩放级别
+		// 设置地图中心点及缩放级别
 		MapStatusUpdate su = MapStatusUpdateFactory.newLatLngZoom(
 			locationLatLng, 16);
 		map.animateMapStatus(su);
 
-		//设置地图覆盖点点击监听事件
+		// 设置地图覆盖点点击监听事件
 		map.setOnMarkerClickListener(new MarkerListener());
-		//设置地图点击监听事件
+		// 设置地图点击监听事件
 		map.setOnMapClickListener(new MapClickListener());
-		//判断用户是否在景区
+		// 判断用户是否在景区
+		
 		setMark(location);
+
+		
 	    }
 	}
     }
-    //地图点击事件回掉接口
+
+    // 地图点击事件回掉接口
     private final class MapClickListener implements OnMapClickListener
     {
 
@@ -235,7 +248,8 @@ public class MapUtil
 	}
 
     }
-    //覆盖点点击回掉接口
+
+    // 覆盖点点击回掉接口
     private final class MarkerListener implements OnMarkerClickListener
     {
 
@@ -248,9 +262,9 @@ public class MapUtil
 	    {
 		setPopMarker(arg0.getPosition(), bundle);
 	    }
-	    else if(type.equals("jingDian"))
+	    else if (type.equals("jingDian"))
 	    {
-		setPopMarker(arg0.getPosition(),bundle);
+		setPopMarker(arg0.getPosition(), bundle);
 	    }
 
 	    return true;
@@ -278,27 +292,37 @@ public class MapUtil
 	cli.stop();
 	map.setMyLocationEnabled(false);
     }
-    //判断用户进没进入景点
+
+    // 判断用户进没进入景点
     private void setMark(BDLocation location)
     {
 	List<JSONObject> locationSpot = aService.judgeLocation(location.getLongitude(), location.getLatitude());
 	JSONObject currentSpot = locationSpot.get(0);
-	
+
 	try
 	{
-	    for(JSONObject j : locationSpot)
+
+	    for (JSONObject j : locationSpot)
 	    {
-		setJingDianPointer(j.getInt("ID"));
-		setSpotArea(j.getInt("ID"));
+		try
+		{
+		    setJingDianPointer(j.getInt("ID"));
+		    setSpotArea(j.getInt("ID"));
+		}
+		catch (Exception e)
+		{
+		    // TODO Auto-generated catch block
+		    e.printStackTrace();
+		}
 	    }
-	    
+
 	    if (currentSpot.getInt("distance") < 1000)
 	    {
-		vUtil.playAudio("您当前所在的景区是"+currentSpot.getString("name"));
+		vUtil.playAudio("您当前所在的景区是" + currentSpot.getString("name"));
 	    }
-	    else
-	    {	
-	      vUtil.playAudio("您当前不在任何景区");
+	   else
+	    {
+		vUtil.playAudio("您当前不在任何景区");
 	    }
 	}
 	catch (Exception e)
@@ -306,23 +330,26 @@ public class MapUtil
 	    e.printStackTrace();
 	}
     }
-    
-    //表示景区
-    private void setSpotArea(int spotId)
+
+    // 表示景区
+    public void setSpotArea(int spotId)
     {
-	List<JSONObject> spotAroundPoints = aService.getSpotAroundPointsBysoptId(spotId);
+	List<JSONObject> spotAroundPoints = aService
+		.getSpotAroundPointsBysoptId(spotId);
 	List<LatLng> pts = null;
-	//填充颜色数组
-	int colors[] = { 0xAA51c6f4,0xAA0ad15a,0xAAc0d108,0xAAef7ada};
-	if(spotAroundPoints.size()>=5)
+	// 填充颜色数组
+	int colors[] =
+	{ 0xAA51c6f4, 0xAA0ad15a, 0xAAc0d108, 0xAAef7ada };
+	if (spotAroundPoints.size() >= 5)
 	{
-	    pts = new ArrayList<LatLng>();  
-	    for(JSONObject j : spotAroundPoints)
+	    pts = new ArrayList<LatLng>();
+	    for (JSONObject j : spotAroundPoints)
 	    {
 		LatLng pt = null;
 		try
 		{
-		    pt = new LatLng(j.getDouble("latitude") ,  j.getDouble("longitude"));
+		    pt = new LatLng(j.getDouble("latitude"),
+			    j.getDouble("longitude"));
 		}
 		catch (JSONException e)
 		{
@@ -330,22 +357,22 @@ public class MapUtil
 		    e.printStackTrace();
 		}
 		pts.add(pt);
-		
+
 	    }
-	    i = i%4;
+	    i = i % 4;
 	    i++;
-	    OverlayOptions polygonOption = new PolygonOptions().points(pts).stroke(new Stroke(5, colors[i])).fillColor(colors[i]);  
-	    //在地图上添加多边形Option，用于标识景区位置  
+	    OverlayOptions polygonOption = new PolygonOptions().points(pts)
+		    .stroke(new Stroke(5, colors[i])).fillColor(colors[i]);
+	    // 在地图上添加多边形Option，用于标识景区位置
 	    map.addOverlay(polygonOption);
 	}
-  
- }
 
-    //标识景点所在区域
-    private void setJingDianPointer(int ID) throws JSONException
+    }
+
+    // 标识景点所在区域
+    public void setJingDianPointer(int ID)
     {
-	List<JingDianBean> jingDians = aService
-		.getAllJingDianBySpotId(ID);
+	List<JingDianBean> jingDians = aService.getAllJingDianBySpotId(ID);
 	if (jingDians != null)
 	{
 	    for (JingDianBean i : jingDians)
@@ -354,11 +381,36 @@ public class MapUtil
 		Bundle bundle = new Bundle();
 		bundle.putString("type", "jingDian");
 		bundle.putString("intro", i.getIntro());
-		bundle.putString("name",i.getName() );
+		bundle.putString("name", i.getName());
 		bundle.putInt("id", i.getId());
 		setMapMarker(R.drawable.am001_map_spot, latlng, bundle);
 	    }
 	}
+    }
+
+    // 设置地图中心点级缩放级别
+    public void setMapCenter(int spotId, int zoom)
+    {
+	List<JSONObject> spotAroundPoints = aService
+		.getSpotAroundPointsBysoptId(spotId);
+	if (spotAroundPoints != null)
+	{
+	    JSONObject center = spotAroundPoints.get(0);
+	    MapStatusUpdate su = null;
+	    try
+	    {
+		su = MapStatusUpdateFactory.newLatLngZoom(
+			new LatLng(center.getDouble("latitude"), center
+				.getDouble("longitude")), 16);
+	    }
+	    catch (JSONException e)
+	    {
+		// TODO Auto-generated catch block
+		e.printStackTrace();
+	    }
+	    map.animateMapStatus(su);
+	}
+
     }
 
 }
